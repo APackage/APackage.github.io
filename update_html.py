ee83f746a805f8ca772bde6c865598daa8a791f7
APackage <mesflit@gmail.com>
2025-01-12 06:49:26 +0000

    Update update_html.py

--- update_html.py
+++ update_html.py
@@ -1,31 +1,38 @@
+import json
 import os
 
-# apps.list dosyasını oku
+# apps.list JSON dosyasının yolu
 apps_list_file = 'apps.list'
 
-# Yeni öğeleri ekleyeceğimiz index.html dosyasının yolu
+# index.html dosyasının yolu
 html_file = 'index.html'
 
 # apps.list dosyasını oku
 with open(apps_list_file, 'r') as f:
-    apps = f.readlines()
+    apps = json.load(f)
 
 # index.html dosyasını oku
 with open(html_file, 'r') as f:
     html_content = f.readlines()
-
-# HTML içeriğini kontrol et
-print("index.html içeriği:")
-print(''.join(html_content))
 
 try:
     # <ul> etiketinin olduğu satırı bul
     ul_start = html_content.index('<ul>\n') + 1
     ul_end = html_content.index('</ul>\n')
 
-    # <ul> arasındaki eski içeriği sil
-    # Mevcut listeyi yeni öğelerle değiştiriyoruz
-    new_apps = [f"<li>\n    <img src='{app.strip()}'>\n    <a href='/apps/{app.strip().lower()}/index.html' target='_blank'>{app.strip()}</a>\n    <a class='cat'>Game</a>\n    <a class='cat'>Emulator</a>\n</li>\n" for app in apps]
+    # Yeni uygulamaları oluştur
+    new_apps = []
+    for app in apps:
+        app_html = f"""
+        <li>
+            <img src="{app['image']}">
+            <a href="{app['url']}" target="_blank">{app['name']}</a>
+        """
+        # Kategorileri ekle
+        for category in app['categories']:
+            app_html += f'<a class="cat">{category}</a>'
+        app_html += "</li>\n"
+        new_apps.append(app_html)
 
     # Güncellenmiş HTML içeriğini oluştur
     html_content = html_content[:ul_start] + new_apps + html_content[ul_end:]
